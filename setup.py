bd0a44242c21802b329b33dee01f89a9fa190adb
andriikobyakovskiy <andrii.kobyakovskiy@gmail.com>
2023-02-10 10:45:51 +0100

    Added basic setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='battleship',
+      version='0.0.1',
+      packages=find_packages(
+            include=['battleship/*'],
+            exclude=['battleship.tests'],
+      ),
+)
